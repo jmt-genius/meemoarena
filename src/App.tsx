f0cb58e4490547ba1261cae85ef656fe92d65bd1
J M Tarun <tarunjm23@gmail.com>
2025-04-26 22:18:42 +0530

    added some ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
 import { isValidSuiObjectId } from "@mysten/sui/utils";
 import { Box, Container, Flex, Heading } from "@radix-ui/themes";
@@ -5,7 +6,7 @@ import { useState } from "react";
 import { Counter } from "./Counter";
 import { CreateCounter } from "./CreateCounter";
 import LetterGlitch from './blocks/Backgrounds/LetterGlitch/LetterGlitch';
-
+import StakePage from './components/StakePage';
 
 function App() {
   const currentAccount = useCurrentAccount();
@@ -15,15 +16,27 @@ function App() {
   });
 
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/counter" element={<CounterPage />} />
+        <Route path="/stake" element={<StakePage />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
     <>
-        
-<LetterGlitch
-  glitchSpeed={50}
-  centerVignette={true}
-  outerVignette={false}
-  smooth={true}
-  glitchColors={["#FF0000", "#00FF00", "#0000FF"]}
-/>
+      <LetterGlitch
+        glitchSpeed={50}
+        centerVignette={true}
+        outerVignette={false}
+        smooth={true}
+        glitchColors={["#FF0000", "#00FF00", "#0000FF"]}
+      />
       <Flex
         position="sticky"
         px="4"
@@ -48,28 +61,40 @@ function App() {
           px="4"
           style={{ background: "var(--gray-a2)", minHeight: 500 }}
         >
-          {currentAccount ? (
-            counterId ? (
-              <Counter id={counterId} />
-            ) : (
-              <CreateCounter
-                onCreated={(id) => {
-                  window.location.hash = id;
-                  setCounter(id);
-                }}
-              />
-            )
-          ) : (
-            <>
-              <Heading>Please connect your wallet</Heading>
-              <h1 className="text-3xl font-bold underline">
-                Hello world!
-              </h1>
-            </>
-          )}
+          <Heading>Please connect your wallet</Heading>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
         </Container>
       </Container>
     </>
+  );
+}
+
+function CounterPage() {
+  const currentAccount = useCurrentAccount();
+  const [counterId, setCounter] = useState(() => {
+    const hash = window.location.hash.slice(1);
+    return isValidSuiObjectId(hash) ? hash : null;
+  });
+
+  return (
+    <Container>
+      {currentAccount ? (
+        counterId ? (
+          <Counter id={counterId} />
+        ) : (
+          <CreateCounter
+            onCreated={(id) => {
+              window.location.hash = id;
+              setCounter(id);
+            }}
+          />
+        )
+      ) : (
+        <Heading>Please connect your wallet</Heading>
+      )}
+    </Container>
   );
 }
 
---
+++ src/components/StakePage.tsx
@@ -0,0 +1,13 @@
+import { Container, Heading } from "@radix-ui/themes";
+
+function StakePage() {
+  return (
+    <Container>
+      <Heading>Stake Your Tokens</Heading>
+      <p>Here you can stake your tokens to earn rewards.</p>
+      {/* Add more content and functionality as needed */}
+    </Container>
+  );
+}
+
+export default StakePage; 
